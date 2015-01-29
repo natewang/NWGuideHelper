71a97d0f26cb3b024976fb6505a3ceda32ccd85f
wangxu <admin@example.com>
2015-01-29 12:15:40 +0800

    Initial Commit

---
+++ NWGuideHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NWGuideHelper
+//
+//  Created by nate on 29/1/15.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NWGuideHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NWGuideHelper
+//
+//  Created by nate on 29/1/15.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
